e1be73a40036e8b987e05486d718f05449c9d46b
pedrindenard <pedrominuzzi21@gmail.com>
2021-02-04 08:17:11 -0300

    Adicionando as receitas no resumo

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,10 +3,12 @@ package br.com.alura.financask.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -18,7 +20,19 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         val transacoes : List<Transacao> = transaçõesDeExemplo()
 
+        adicionaReceitaNoResumo(transacoes)
+
         configuraLista(transacoes)
+    }
+
+    private fun adicionaReceitaNoResumo(transacoes: List<Transacao>) {
+        var totalReceita = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.RECEITA) {
+                totalReceita = totalReceita.plus(transacao.valor)
+            }
+        }
+        resumo_card_receita.text = totalReceita.formataParaBrasileiro()
     }
 
     private fun configuraLista(transacoes: List<Transacao>) {
