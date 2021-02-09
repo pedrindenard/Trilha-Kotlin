060be9790c99ef446bca2a94a76099cd13414242
pedrindenard <pedrominuzzi21@gmail.com>
2021-02-09 10:23:14 -0300

    Inicializacao atrasada com lateinit

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -10,7 +10,7 @@ import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
 import java.math.BigDecimal
 
-class ResumoView(private val view: View?,
+class ResumoView(private val view: View,
                  context: Context,
                  transacoes: List<Transacao>) {
 
@@ -26,33 +26,27 @@ class ResumoView(private val view: View?,
 
     private fun adicionaReceita() {
         val totalReceita = resumo.receita
-        view?.let {
-            with(it.resumo_card_receita) {
+            with(view.resumo_card_receita) {
                 setTextColor(corReceita)
                 text = totalReceita.formataParaBrasileiro()
             }
-        }
     }
 
     private fun adicionaDespesa() {
         val totalDespesa = resumo.despesa
-        view?.let {
-            with (it.resumo_card_despesa) {
+            with (view.resumo_card_despesa) {
                 setTextColor(corDespesa)
                 text = totalDespesa.formataParaBrasileiro()
             }
-        }
     }
 
     private fun adicionaTotal() {
         val total = resumo.total
         val cor = corPor(total)
-        view?.let {
-            with (it.resumo_card_total) {
+            with (view.resumo_card_total) {
                 setTextColor(cor)
                 text = total.formataParaBrasileiro()
             }
-        }
     }
 
     private fun corPor(valor: BigDecimal): Int {
